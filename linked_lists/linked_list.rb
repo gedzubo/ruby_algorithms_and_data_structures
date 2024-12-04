c0c74ce69b3d219417038f62e74cddd395144985
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-12-04 15:56:15 +0000

    Adding insert and delete methods

--- linked_lists/linked_list.rb
+++ linked_lists/linked_list.rb
@@ -100,5 +100,37 @@ class LinkedList
     true
   end
 
+  def insert(index, value)
+    return false if index.negative? || index >= lenght
+    return unshift(value) if index.zero?
+    return push(value) if index == lenght
+
+    new_node = Node.new(value)
+    previous_node = get(index - 1)
+
+    new_node.next_node = previous_node.next_node
+    previous_node.next_node = new_node
+
+    @lenght += 1
+
+    true
+  end
+
+  def delete(index)
+    return nil if index.negative? || index >= lenght
+    return shift(value) if index.zero?
+    return pop if index == lenght - 1
+
+    previous_node = get(index - 1)
+    node_to_delete = previous_node.next
+
+    previous_node.next_node = node_to_deletet.next_node
+    node_to_delete.next_node = nil
+
+    @lenght -= 1
+
+    node_to_delete
+  end
+
   attr_accessor :head, :tail, :lenght
 end
