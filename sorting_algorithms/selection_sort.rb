9376bcf20b6b97766458b79e44c53e26f923f87e
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-12-30 12:45:36 +0000

    Adding selection sort

---
+++ sorting_algorithms/selection_sort.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def selection_sort(array)
+  n = array.length
+
+  (n - 1).times do |i|
+    min_index = i
+
+    (i + 1).upto(n - 1) do |j|
+      min_index = j if array[j] < array[min_index]
+    end
+
+    array[i], array[min_index] = array[min_index], array[i] if min_index != i
+  end
+
+  array
+end
---
+++ sorting_algorithms/spec/selection_sort_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../selection_sort'
+
+describe '#selection_sort' do
+  context 'when the array is empty' do
+    it 'returns an empty array' do
+      expect(selection_sort([])).to eq([])
+    end
+  end
+
+  context 'when the array has one element' do
+    it 'returns the array' do
+      expect(selection_sort([1])).to eq([1])
+    end
+  end
+
+  context 'when the array has multiple elements' do
+    it 'returns the sorted array' do
+      expect(selection_sort([3, 2, 1, 5, 4])).to eq([1, 2, 3, 4, 5])
+    end
+  end
+end
