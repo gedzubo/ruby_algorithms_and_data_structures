7e41a3cf8a8d6cd50b9d86f0be23326a7778f485
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-12-31 15:00:02 +0000

    Adding insertion sort

---
+++ sorting_algorithms/insertion_sort.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+def insertion_sort(array)
+  n = array.length
+
+  1.upto(n - 1) do |i|
+    temp = array[i]
+    j = i - 1
+
+    while j >= 0 && array[j] > temp
+      array[j + 1] = array[j]
+      j -= 1
+    end
+
+    array[j + 1] = temp
+  end
+
+  array
+end
