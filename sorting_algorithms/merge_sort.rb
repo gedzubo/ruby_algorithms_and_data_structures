32d2953d0b80dfc8136f5ff8862ccc40ffad3f54
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-01-01 10:47:07 +0000

    Adding merge sort

---
+++ sorting_algorithms/merge_sort.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+def merge_sort(array)
+  return array if array.length <= 1
+
+  mid = array.length / 2
+  left = merge_sort(array[0...mid])
+  right = merge_sort(array[mid..])
+
+  merge(left, right)
+end
+
+def merge(array1, array2)
+  merged = []
+  i = 0
+  j = 0
+
+  while i < array1.length && j < array2.length
+    if array1[i] < array2[j]
+      merged.push(array1[i])
+      i += 1
+    else
+      merged.push(array2[j])
+      j += 1
+    end
+  end
+
+  while i < array1.length
+    merged.push(array1[i])
+    i += 1
+  end
+
+  while j < array2.length
+    merged.push(array2[j])
+    j += 1
+  end
+
+  merged
+end
---
+++ sorting_algorithms/spec/merge_sort_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../merge_sort'
+
+# Space complexity: O(n)
+# Time complexity: O(n log n)
+describe '#merge_sort' do
+  context 'when the array is empty' do
+    it 'returns an empty array' do
+      expect(merge_sort([])).to eq([])
+    end
+  end
+
+  context 'when the array has one element' do
+    it 'returns the array' do
+      expect(merge_sort([1])).to eq([1])
+    end
+  end
+
+  context 'when the array has multiple elements' do
+    it 'returns the sorted array' do
+      expect(merge_sort([3, 2, 1, 5, 4])).to eq([1, 2, 3, 4, 5])
+    end
+  end
+end
