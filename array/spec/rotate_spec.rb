521bc6e16614365473aa78aabc4f5829d81ad1c0
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-03-04 18:00:25 +0000

    Adding rotate exercise to the array

---
+++ array/spec/rotate_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../rotate'
+
+describe '#rotate' do
+  test_cases = [
+    { input: [[1, 2, 3, 4, 5], 2], expected: [4, 5, 1, 2, 3] },
+    { input: [[1, 2, 3, 4, 5], 3], expected: [3, 4, 5, 1, 2] },
+    { input: [[1, 2, 3, 4, 5], 1], expected: [5, 1, 2, 3, 4] },
+    { input: [[1, 2, 3, 4, 5], 5], expected: [1, 2, 3, 4, 5] },
+    { input: [[1, 2, 3, 4, 5], 6], expected: [5, 1, 2, 3, 4] }
+  ]
+
+  test_cases.each_with_index do |test_case, index|
+    it "rotates an array by a specified number of positions (case #{index + 1})" do
+      expect(rotate(*test_case[:input])).to eq(test_case[:expected])
+    end
+  end
+end
